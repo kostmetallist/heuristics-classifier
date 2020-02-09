8b65438984ce553be7e847f6641dc3c7e9361544
konstantin <kostmetallist@gmail.com>
2020-02-09 23:46:10 +0300

    Implement data source selection

--- core/log_processor.go
+++ core/log_processor.go
@@ -5,7 +5,11 @@ import (
 	"io/ioutil"
 	"github.com/kostmetallist/heuclassifier/error"
 	"github.com/kostmetallist/heuclassifier/gsheets"
+	"os"
 )
+
+var dataSources = [...]string{"local CSV", "local XLSX", "google spreadsheets"}
+var chosenDataSource string
 
 func readFile2String(ch chan<- string, path string) {
 	data, err := ioutil.ReadFile(path)
@@ -21,5 +25,29 @@ func ProcessLogData() {
 	data = <- chWelcomeMsg
 	fmt.Print(string(data))
 
-	gsheets.GetGoogleSheetData("gsheets/secret.json", "gsheets/params.json")
+	fmt.Println("Please specify an event log data source.", 
+		"Possible choices are:")
+	for i, entry := range dataSources {
+		fmt.Println(fmt.Sprintf("[%d] %s", i, entry))
+	}
+
+	var chosenDataSourceIdx int
+	for isDataSourceCorrect := false; isDataSourceCorrect == false; {
+
+		fmt.Print("Your choice: ")
+		fmt.Scanf("%d", &chosenDataSourceIdx)
+		if chosenDataSourceIdx < 0 || chosenDataSourceIdx >= len(dataSources) {
+			fmt.Fprintln(os.Stderr, "An incorrect value has been passed.",
+				"Acceptable range is", 
+				fmt.Sprintf("[0-%d]", len(dataSources)-1))
+			continue
+		} 
+		
+		isDataSourceCorrect = true
+		chosenDataSource = dataSources[chosenDataSourceIdx]
+	}
+
+	if (chosenDataSource == "google spreadsheets") {
+		gsheets.GetGoogleSheetData("gsheets/secret.json", "gsheets/params.json")
+	}
 }
