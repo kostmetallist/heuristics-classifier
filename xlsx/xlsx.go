a5900a470f560c7829163babcfd45be909d73e1d
konstantin <kostmetallist@gmail.com>
2020-02-12 23:03:22 +0300

    Implement limitation for XLSX reading

--- xlsx/xlsx.go
+++ xlsx/xlsx.go
@@ -12,24 +12,31 @@ func GetXlsxData(configFilePath string) [][]string {
 	logging.HCLogger.Println("fetching configuration from", configFilePath)
 	params := json.RetrieveJsonData(configFilePath)
 	xlsxFilePath := params["xlsxFileLocation"].(string)
-
 	// limitation is applied when the value is non negative
-	// readLinesLimitation := params["limitReadLinesTo"].(int)
-	// TODO implement read lines limitation
+	readLimit := int(params["limitReadLinesTo"].(float64))
+
 	logging.HCLogger.Println("loading XLSX file", xlsxFilePath)
 	xlsxFile, err := xlsx.OpenFile(xlsxFilePath)
 	error.CheckError(err)
 
+	processedLines := 0
 	result := [][]string{}
 	// note: reads continiously from all the sheets presented in a document
 	for _, sheet := range xlsxFile.Sheets {
 		for _, row := range sheet.Rows {
+
+			if processedLines > readLimit {
+				logging.HCLogger.Println("XLSX lines read limit exceeded:", 
+					"reached", readLimit, "lines")
+				continue
+			}
 
 			rowData := []string{}
 			for _, cell := range row.Cells {
 				rowData = append(rowData, cell.String())
 			}
 			result = append(result, rowData)
+			processedLines += 1
 		}
 	}
 
