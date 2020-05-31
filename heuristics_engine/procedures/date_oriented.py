e9131c2ae4172bbcd94db38357674c0fdfa5ced1
konstantin <kostmetallist@gmail.com>
2020-05-31 07:14:39 +0300

    Provide date_oriented temporal sequence retrieval

--- heuristics_engine/procedures/date_oriented.py
+++ heuristics_engine/procedures/date_oriented.py
@@ -1,14 +1,92 @@
+from datetime import datetime as dt
+import dateutil.parser as dateparser
+
 import logger as lg
 from datastructures.statement import Statement
 from procedures.heuristic_base import HeuristicBase
-
 
 DEFAULT_STATEMENTS_DUMP_FILE = 'output/statements_dump/date_oriented.txt'
 logger = lg.get_logger('HEL')
 
 
 class DateOriented(HeuristicBase):
+
+    PATTERNS = [
+        '%Y %b %d',
+        '%Y %b %d %H:%M:%S',
+        '%Y %B %d',
+        '%Y %B %d %H:%M:%S',
+        '%Y/%m/%d',
+        '%Y/%m/%d %H:%M:%S',
+        '%x',
+    ]
+
+    @staticmethod
+    def _try_retrieve_timestamp(value: int):
+        try:
+            return dt.fromtimestamp(value)
+        except OSError:
+            logger.error(f'given value ({value}) is too large for timestamp')
+            return
+
+    @staticmethod
+    def _try_datetime_parsing(raw: str):
+        try:
+            return dateparser.isoparse(raw)
+        except ValueError:
+            parsed = None
+
+        for pattern in DateOriented.PATTERNS:
+            try:
+                parsed = dt.strptime(raw, pattern)
+                break
+            except ValueError:
+                continue
+
+        logger.error(f'unable to process string "{raw}" as a date[time]')
+        return parsed 
+
+    @staticmethod
+    def _get_temporal_sequence(values):
+        '''
+        Convert given sequence into the list of `datetime` objects.
+        '''
+        result = []
+        if not values:
+            logger.error('cannot process empty sequence')
+            return result
+        else:
+            underlying_type = type(values[0])
+
+        logger.info(f'converting sequence of "{underlying_type}" items into '
+                    + 'temporal sequence')
+        # TODO set limit for incorrectly parsed entries after which this function
+        # will be aborted
+        for item in values:
+            if underlying_type is int:
+                temporal = DateOriented._try_retrieve_timestamp(item)
+                if temporal:
+                    result.append(temporal)
+            elif underlying_type is str:
+                try:
+                    temporal = DateOriented._try_retrieve_timestamp(float(item))
+                    if temporal:
+                        result.append(temporal)
+                except ValueError:
+                    parsed = DateOriented._try_datetime_parsing(item)
+                    if parsed:
+                        result.append(parsed)
+            else:
+                logger.error(f'provided type ({underlying_type}) cannot be '
+                             'transformed into temporal-like one, aborting...')
+                break
+
+        return result
+
     def infer_statement_for_integer(self, values):
+        logger.info('getting specific statements for INTEGER type attribute')
+        datetimes = self._get_temporal_sequence(values)
+        print(datetimes)
         return None
 
     def infer_statement_for_float(self, values):
@@ -19,6 +97,8 @@ class DateOriented(HeuristicBase):
 
     def infer_statement_for_string(self, values):
         logger.info('getting specific statements for STRING type attribute')
+        datetimes = self._get_temporal_sequence(values)
+        print(datetimes)
         return None
 
     def process_messages(self, dump_file=DEFAULT_STATEMENTS_DUMP_FILE):
